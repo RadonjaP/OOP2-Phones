5a1b6697c299b016ab2265be1c2379c1d956e932
Radonja <rudonja8@gmail.com>
2016-05-31 17:46:53 +0200

    Entities generated (Nenad Misic and Radonja Prelevic)

---
+++ .metadata/.plugins/org.eclipse.core.resources/.history/ce/005a4e86442700161210ea8a189df957
@@ -0,0 +1,90 @@
+package entity;
+
+import java.io.Serializable;
+import javax.persistence.*;
+import java.sql.Time;
+import java.util.Date;
+
+
+/**
+ * The persistent class for the COMMENT database table.
+ * 
+ */
+@Entity
+@NamedQuery(name="Comment.findAll", query="SELECT c FROM Comment c")
+public class Comment implements Serializable {
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private int id;
+
+	private String content;
+
+	@Temporal(TemporalType.DATE)
+	private Date date;
+
+	private Time time;
+
+	//bi-directional many-to-one association to Auction
+	@ManyToOne
+	@JoinColumn(name="ID_AUCTION")
+	private Auction auction;
+
+	//bi-directional many-to-one association to User
+	@ManyToOne
+	@JoinColumn(name="ID_USER")
+	private User user;
+
+	public Comment() {
+	}
+
+	public int getId() {
+		return this.id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getContent() {
+		return this.content;
+	}
+
+	public void setContent(String content) {
+		this.content = content;
+	}
+
+	public Date getDate() {
+		return this.date;
+	}
+
+	public void setDate(Date date) {
+		this.date = date;
+	}
+
+	public Time getTime() {
+		return this.time;
+	}
+
+	public void setTime(Time time) {
+		this.time = time;
+	}
+
+	public Auction getAuction() {
+		return this.auction;
+	}
+
+	public void setAuction(Auction auction) {
+		this.auction = auction;
+	}
+
+	public User getUser() {
+		return this.user;
+	}
+
+	public void setUser(User user) {
+		this.user = user;
+	}
+
+}
---
+++ PhoneServerTeam6/src/entity/Phone.java
@@ -0,0 +1,136 @@
+package entity;
+
+import java.io.Serializable;
+import javax.persistence.*;
+
+
+/**
+ * The persistent class for the PHONE database table.
+ * 
+ */
+@Entity
+@NamedQuery(name="Phone.findAll", query="SELECT p FROM Phone p")
+public class Phone implements Serializable {
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private int id;
+
+	@Column(name="BATTERY_CAPACITY")
+	private int batteryCapacity;
+
+	@Column(name="CPU_SPEED")
+	private double cpuSpeed;
+
+	private double height;
+
+	private int memory;
+
+	@Column(name="PICTURE_ADDRESS")
+	private String pictureAddress;
+
+	private int price;
+
+	@Column(name="RESOLUTION_X")
+	private int resolutionX;
+
+	@Column(name="RESOLUTION_Y")
+	private int resolutionY;
+
+	private double weight;
+
+	private double width;
+
+	public Phone() {
+	}
+
+	public int getId() {
+		return this.id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public int getBatteryCapacity() {
+		return this.batteryCapacity;
+	}
+
+	public void setBatteryCapacity(int batteryCapacity) {
+		this.batteryCapacity = batteryCapacity;
+	}
+
+	public double getCpuSpeed() {
+		return this.cpuSpeed;
+	}
+
+	public void setCpuSpeed(double cpuSpeed) {
+		this.cpuSpeed = cpuSpeed;
+	}
+
+	public double getHeight() {
+		return this.height;
+	}
+
+	public void setHeight(double height) {
+		this.height = height;
+	}
+
+	public int getMemory() {
+		return this.memory;
+	}
+
+	public void setMemory(int memory) {
+		this.memory = memory;
+	}
+
+	public String getPictureAddress() {
+		return this.pictureAddress;
+	}
+
+	public void setPictureAddress(String pictureAddress) {
+		this.pictureAddress = pictureAddress;
+	}
+
+	public int getPrice() {
+		return this.price;
+	}
+
+	public void setPrice(int price) {
+		this.price = price;
+	}
+
+	public int getResolutionX() {
+		return this.resolutionX;
+	}
+
+	public void setResolutionX(int resolutionX) {
+		this.resolutionX = resolutionX;
+	}
+
+	public int getResolutionY() {
+		return this.resolutionY;
+	}
+
+	public void setResolutionY(int resolutionY) {
+		this.resolutionY = resolutionY;
+	}
+
+	public double getWeight() {
+		return this.weight;
+	}
+
+	public void setWeight(double weight) {
+		this.weight = weight;
+	}
+
+	public double getWidth() {
+		return this.width;
+	}
+
+	public void setWidth(double width) {
+		this.width = width;
+	}
+
+}
